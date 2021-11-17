16b52775f9371f13f6c0fcbcc19b69e9570b0ce9
Ubuntu <-C>
2021-11-18 01:38:41 +0200

    Edited the .eslintrc.json file and installed additional dependencies.

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { StatusBar, StyleSheet, View } from 'react-native';
 import { RootFile } from './src/RootFile';
+
+{/*Файл .Eslintrc пытался настроить, но он у меня, что-то не работает. 
+  Наверное что-то упускаю, так как с правилами eslint'a, почти не работал.
+*/}
 const App = () => {
   return (
     <View style={styles.container}>
