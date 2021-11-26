c51d9a4ac400678f46b6e54635f30585f56f7906
Ubuntu <-C>
2021-11-26 20:26:49 +0200

    Added array objects to ListOfPeople.

--- src/components/ListOfPeople.js
+++ src/components/ListOfPeople.js
@@ -1,23 +1,72 @@
 export const ListOfPeople = [
     {
-        id: "1",
+        "id": "1",
         "name": "Mark Zuckerberg",
         "img": "https://upload.wikimedia.org/wikipedia/commons/f/fe/Mark_Zuckerberg_em_setembro_de_2014.jpg",
         "company": "Facebook",
         "description": "Zuckerberg was born in White Plains, New York, to a Jewish family of physicians.Mark's passion for programming began to actively manifest itself in high school. By the ninth grade, he released a computer version of the board game \"Risk\", which was very popular at the time. "
     },
     {
-        id: "2",
+        "id": "2",
         "name": "Bill Gates",
         "img": "https://upload.wikimedia.org/wikipedia/commons/a/a0/Bill_Gates_2018.jpg",
         "company": "Microsoft",
         "description": "American businessman, investor, writer, philanthropist, founder of Microsoft."
     },
     {
-        id: "3",
+        "id": "3",
         "name": "Bjarne Stroustrup",
         "img": "https://upload.wikimedia.org/wikipedia/commons/d/da/BjarneStroustrup.jpg",
         "company": "Founder of C++",
         "description": "Björn Straustrup developed a high-level C ++ programming language. Straustrup pioneered the use of object-oriented and general techniques in the creation of programs in which efficiency is a key property."
     },
+    {
+        "id": "4",
+        "name": "Steve Jobs",
+        "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Steve_Jobs_Headshot_2010-CROP.jpg/800px-Steve_Jobs_Headshot_2010-CROP.jpg",
+        "company": "Apple Inc.",
+        "description": "He was the founder, board member and CEO (CEO) of Apple Inc. Jobs also previously served as CEO of Pixar Animation Studios ... "
+    },
+    {
+        "id": "5",
+        "name": "Linus Torvalds",
+        "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/LinuxCon_Europe_Linus_Torvalds_03.jpg/800px-LinuxCon_Europe_Linus_Torvalds_03.jpg",
+        "company": "OS Linux",
+        "description": "Finnish-American programmer, who started the development of the Linux operating system kernel, is its chief architect, as well as the author of the most famous distributed version control system Git. In 2012 he was awarded the Millennium Technology Prize for the creation of Linux."
+    },
+    {
+        "id": "6",
+        "name": "Pavel Durov",
+        "img": "https://upload.wikimedia.org/wikipedia/commons/6/68/TechCrunch_Disrupt_Europe_Berlin_2013_%2810536888854%29_%28cropped%29.jpg",
+        "company": "VK, OK, Telegram",
+        "description": "Russian programmer, one of the creators and former CEO of social networks \"VK\"..."
+    },
+    {
+        "id": "7",
+        "name": "Brendan Eich",
+        "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Brendan_Eich_Mozilla_Foundation_official_photo.jpg/800px-Brendan_Eich_Mozilla_Foundation_official_photo.jpg",
+        "company": "Founder of JavaScript",
+        "description": "Programmer, author of JavaScript scripting language. He served as CTO at Mozilla Corporation, and in March 2014 was appointed CEO of Mozilla Corporation."
+    },
+    {
+        "id": "8",
+        "name": "Jan Koum",
+        "img": "https://upload.wikimedia.org/wikipedia/commons/0/06/Jan_Koum_%28cropped%29.jpg",
+        "company": "WhatsApp",
+        "description": "American programmer and entrepreneur of Ukrainian-Jewish origin, emigrant from Ukraine, developer of WhatsApp messenger, executive director of Facebook since 2014."
+    },
+    {
+        "id": "9",
+        "name": "Dennis Ritchie",
+        "img": "https://upload.wikimedia.org/wikipedia/commons/2/23/Dennis_Ritchie_2011.jpg",
+        "company": "\"C\" programming language.",
+        "description": "American computer scientist, known as the developer of the C programming language and influencing other programming languages."
+    },
+    {
+        "id": "10",
+        "name": "Timothy John Berners-Lee",
+        "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Tim_Berners-Lee_April_2009.jpg/800px-Tim_Berners-Lee_April_2009.jpg",
+        "company": "Founder of HTTP, HTML, URI",
+        "description": "British computer scientist, creator of HTTP, HTML, URI and author of other developments in the field of information technology, founder and chairman of the W3C consortium, chief developer of the World Wide Web (with Robert Cayo)."
+    }
 ]
--- src/screens/home/style.js
+++ src/screens/home/style.js
@@ -1,7 +1,8 @@
 import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        backgroundColor: 'silver'
     },
     title: {
         alignItems: 'center'
