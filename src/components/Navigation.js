de6822fd42a694c29a9ca04203d3c7c33a8486df
Ubuntu <-C>
2021-11-26 17:47:09 +0200

    Added BottomNavigation and transition between them.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { HomeScreen } from '../screens/home/HomeScreen';
 import { ProfileScreen } from '../screens/profile/ProfileScreen';
+import { Settings } from '../screens/settings/Settings';
 export const Navigation = () => {
     const { Navigator, Screen } = createNativeStackNavigator();
+    const BottomTab = createBottomTabNavigator();
+
+    const Home = () => {
+        return (
+            <Navigator>
+                <Screen
+                    name={'HomeScreen'}
+                    component={HomeScreen}
+                    options={{ headerShown: false }} />
+                <Screen
+                    name={'Profile'}
+                    component={ProfileScreen}
+                    options={{ headerTitleAlign: 'center' }} />
+            </Navigator>
+        );
+    };
+
     return (
         <NavigationContainer>
-            <Navigator>
-                <Screen name={'Home'} component={HomeScreen} />
-                <Screen name={'Profile'} component={ProfileScreen} />
-            </Navigator>
+            <BottomTab.Navigator>
+                <BottomTab.Screen
+                    name={'Home'}
+                    component={Home}
+                    options={{ headerShown: false }} />
+                <BottomTab.Screen
+                    name={'Settings'}
+                    component={Settings}
+                    options={{ headerShown: false }} />
+            </BottomTab.Navigator>
         </NavigationContainer>
     );
 };
--- src/screens/profile/styles.js
+++ src/screens/profile/styles.js
@@ -4,6 +4,5 @@ export const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#2c3e50',
     },
 });
