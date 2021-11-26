8e25ddd919b2b5f91b57741317165c0f5b69006a
Ubuntu <-C>
2021-11-26 18:33:30 +0200

    Created a file ListOfPeple data that will be displayed on the screen.

---
+++ src/components/ListOfPeople.js
@@ -0,0 +1,23 @@
+export const ListOfPeople = [
+    {
+        id: "1",
+        "name": "Mark Zuckerberg",
+        "img": "https://upload.wikimedia.org/wikipedia/commons/f/fe/Mark_Zuckerberg_em_setembro_de_2014.jpg",
+        "company": "Facebook",
+        "description": "Zuckerberg was born in White Plains, New York, to a Jewish family of physicians.Mark's passion for programming began to actively manifest itself in high school. By the ninth grade, he released a computer version of the board game \"Risk\", which was very popular at the time. "
+    },
+    {
+        id: "2",
+        "name": "Bill Gates",
+        "img": "https://upload.wikimedia.org/wikipedia/commons/a/a0/Bill_Gates_2018.jpg",
+        "company": "Microsoft",
+        "description": "American businessman, investor, writer, philanthropist, founder of Microsoft."
+    },
+    {
+        id: "3",
+        "name": "Bjarne Stroustrup",
+        "img": "https://upload.wikimedia.org/wikipedia/commons/d/da/BjarneStroustrup.jpg",
+        "company": "Founder of C++",
+        "description": "Björn Straustrup developed a high-level C ++ programming language. Straustrup pioneered the use of object-oriented and general techniques in the creation of programs in which efficiency is a key property."
+    }
+]
