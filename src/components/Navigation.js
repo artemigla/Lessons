9f833054f1dbbaadc7ca858a34cb7e91bfcb13f4
Ubuntu <-C>
2021-11-26 18:00:44 +0200

    Added icons to BottomNavigation.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,13 @@
 import React from 'react';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicon from 'react-native-vector-icons/Ionicons';
 import { HomeScreen } from '../screens/home/HomeScreen';
 import { ProfileScreen } from '../screens/profile/ProfileScreen';
 import { Settings } from '../screens/settings/Settings';
+
 export const Navigation = () => {
     const { Navigator, Screen } = createNativeStackNavigator();
     const BottomTab = createBottomTabNavigator();
@@ -26,15 +29,25 @@ export const Navigation = () => {
 
     return (
         <NavigationContainer>
-            <BottomTab.Navigator>
+            <BottomTab.Navigator initialRouteName={'Home'}>
                 <BottomTab.Screen
                     name={'Home'}
                     component={Home}
-                    options={{ headerShown: false }} />
+                    options={{
+                        headerShown: false,
+                        tabBarIcon: ({ size, color }) => (
+                            <Ionicon name='home' size={size} color={color} />
+                        )
+                    }} />
                 <BottomTab.Screen
                     name={'Settings'}
                     component={Settings}
-                    options={{ headerShown: false }} />
+                    options={{
+                        headerShown: false,
+                        tabBarIcon: ({ size, color }) => (
+                            <Ionicon name='settings' size={size} color={color} />
+                        )
+                    }} />
             </BottomTab.Navigator>
         </NavigationContainer>
     );
