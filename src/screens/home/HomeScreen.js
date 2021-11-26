a0a981bd2f2c1b4c2167f0f570431d7aac6d1e9e
Ubuntu <-C>
2021-11-27 00:26:21 +0200

    Added a loading indicator and made an imitation of page loading.

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -1,29 +1,38 @@
-import React, { useContext } from 'react';
-import { View, Text, TouchableOpacity, FlatList, Image } from 'react-native';
+import React, { useState, useContext } from 'react';
+import { View, Text, TouchableOpacity, FlatList, Image, ActivityIndicator } from 'react-native';
 import { PeopleContext } from '../../components/PeopleContext';
 import { styles } from './style';
+
 export const HomeScreen = ({ navigation }) => {
 
-    const { people } = useContext(PeopleContext)
+    const [isIndicator, setIsIndicator] = useState(false);
+    const { people } = useContext(PeopleContext);
+
+    setTimeout(() => {
+        setIsIndicator(true);
+    }, 1000);
+
     return (
         <View style={styles.container}>
-            <View style={styles.title}>
-                <Text style={styles.text}>Main page</Text>
-            </View>
-            <FlatList
-                data={people}
-                keyExtractor={(item) => item.id.toString()}
-                renderItem={({ item }) => {
-                    return (
-                        <View style={styles.wrapper}>
-                            <Image style={styles.img} source={{ uri: item.img }} />
-                            <TouchableOpacity onPress={() => navigation.navigate('Profile', { item })}>
-                                <Text style={styles.name}>{item.name}</Text>
-                            </TouchableOpacity>
-                        </View>
-                    )
-                }}
-            />
+            {isIndicator ? <View>
+                <View style={styles.title}>
+                    <Text style={styles.text}>Main page</Text>
+                </View>
+                <FlatList
+                    data={people}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({ item }) => {
+                        return (
+                            <View style={styles.wrapper}>
+                                <Image style={styles.img} source={{ uri: item.img }} />
+                                <TouchableOpacity onPress={() => navigation.navigate('Profile', { item })}>
+                                    <Text style={styles.name}>{item.name}</Text>
+                                </TouchableOpacity>
+                            </View>
+                        );
+                    }}
+                />
+            </View> : <ActivityIndicator size={'large'} />}
         </View>
     );
 };
